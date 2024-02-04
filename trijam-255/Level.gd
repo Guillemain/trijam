b55bc2204094445182e0b07f9ff72e7731a7bd42
furan0 <augustin.delecroix@gmail.com>
2024-02-04 15:59:30 +0100

    Bette spawn

--- trijam-255/Level.gd
+++ trijam-255/Level.gd
@@ -3,21 +3,25 @@ extends Node2D
 @export var speed:= 20.0
 @export var isLevelMoving := false
 
-@export_group("Objects to spawn")
+@export_group("Trunk")
+@export var minDelayTrunk := 0.5
+@export var maxDelayTrunk := 2.0
 @export var trunk : PackedScene
-@export_range(0.0, 100.0) var ProbaTrunk := 25
-@export var devil : PackedScene
-@export_range(0.0, 100.0) var ProbaDevil := 33
-@export var innocent : PackedScene
 
-@export_group("Spawn time")
-@export var minDelay := 0.5
-@export var maxDelay := 2.0
+
+
+@export_group("Peon")
+@export var minDelay := 0.3
+@export var maxDelay := 2.5
+@export_range(0.0, 1.0) var ProbaDevil := 0.33
+@export var devil : PackedScene
+@export var innocent : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	startNextSpawnTimer()
+	startNextTrunkSpawnTimer()
+	startNextPeonSpawnTimer()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -33,11 +37,26 @@ func spawnSomething(sceneToSpawn : PackedScene):
 	instance.position = $Spawn.position
 	$ToMove.add_child(instance)
 
-func randomSpawn():
-	spawnSomething(trunk)
-	startNextSpawnTimer()
+func randomPeonSpawn():
+	# Chose what to spawn
+	var random_float = randf()
+	if random_float < ProbaDevil:
+		spawnSomething(devil)
+	else:
+		spawnSomething(innocent)
+		
+	startNextPeonSpawnTimer()
 
-func startNextSpawnTimer():
+func spawnTrunk():
+	spawnSomething(trunk)
+	startNextTrunkSpawnTimer()
+
+func startNextTrunkSpawnTimer():
+	# random time between minTime & MaxTime
+	var rngDelay := minDelayTrunk + randf() * (maxDelayTrunk - minDelayTrunk)
+	$TrunkTimer.start(rngDelay)
+	
+func startNextPeonSpawnTimer():
 	# random time between minTime & MaxTime
 	var rngDelay := minDelay + randf() * (maxDelay - minDelay)
-	$SpawnTimer.start(rngDelay)
+	$PeonTimer.start(rngDelay)
