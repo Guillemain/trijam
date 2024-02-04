cffaccbfa71108d9034e015d7a011d3d590d9584
Aune <34304992+Guillemain@users.noreply.github.com>
2024-02-04 15:06:14 +0100

    Add peon fx and sprite

---
+++ trijam-255/Peon/died_effect.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play_died()
+
+func play_died():
+	$Explosion.visible = true
+	$Ame.visible = true
+	$Explosion.play("default")
+	$Ame.play("default")
+	var tween := get_tree().create_tween()
+	tween.tween_property($Ame,"position",$Ame.position + Vector2.UP * 500,0.5).set_ease(Tween.EASE_IN)
+	tween.tween_callback(queue_free)
