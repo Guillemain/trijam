968c2ae5e48762ba8e8c1845b64b06917a922a82
furan0 <augustin.delecroix@gmail.com>
2024-02-04 16:30:24 +0100

    Now with score

--- trijam-255/GameManager.gd
+++ trijam-255/GameManager.gd
@@ -5,6 +5,9 @@ extends Node2D
 @export var score := 0
 @export var scoreDevil := 100
 @export var scoreDelta := 5
+var timeElapsed := 0.0
+
+signal endOfGame(String)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,17 +16,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# raise score every second
+	timeElapsed += delta
+	if timeElapsed >= 0.25:
+		timeElapsed = 0
+		score += scoreDelta
+
+	%Score.text = str(score)
+	
 
 func PeonSlayed(type : Peon.EPeonType):
 	if type == Peon.EPeonType.DEVIL:
 		print("Killed DEVIL")
+		score += scoreDevil
 	else:
 		print("Killed INNOCENT")
+		endOfGame.emit("You killed an INNOCENT Soul...")
 
 
 func trunkHit():
-	pass
-
-func endOfGame():
-	pass
+	endOfGame.emit("You fell and broke your femur... Outch !")
